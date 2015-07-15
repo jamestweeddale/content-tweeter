7db50721ea6c17e346b7d5ce63e5b19b5813c0f5
James <tweeddale@gmail.com>
2015-07-15 19:09:52 -0400

    Added documentation

--- src/main/java/com/tweeddale/contenttweeter/services/GoogleImageSearch.java
+++ src/main/java/com/tweeddale/contenttweeter/services/GoogleImageSearch.java
@@ -10,15 +10,20 @@ import org.springframework.stereotype.Service;
 
 /**
  * Created by James on 7/3/2015.
+ *
+ * TRhis implementation of ImageSearchService queries Google Image Search via an HTTP request. Results are returned in
+ * a JSON object which is parsed to store only the image URLS in a List.
  */
 @Service
 public class GoogleImageSearch implements ImageSearchService {
 
     private final String searchUrlStr = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=";
 
-    public GoogleImageSearch(){
-    }
-
+    /**
+     *
+     * @param searchStr   The search string used to query for a related image
+     * @return a List of URL strings, each pointing to a related image result from the Googkle image search
+     */
     public List<String> search(String searchStr) {
         StringBuilder builder = new StringBuilder();
 
@@ -40,6 +45,11 @@ public class GoogleImageSearch implements ImageSearchService {
         return parseOutResultImageUrls(builder.toString());
     }
 
+    /**
+     *
+     * @param json the JSON object returned by the Google image search performed by the search() method.
+     * @return List of strings, each one a URL from each search result in the JSON object that was passed-in
+     */
     private List<String> parseOutResultImageUrls(String json){
 
         ArrayList resultsList = new ArrayList();
